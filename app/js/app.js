540bf45517b80d9a9918d06a938a164c2f811bd8
Sarah Cnota <scnota@gmail.com>
2014-07-06 00:44:01 -0500

    add controller with hard coded data and display on page using repeat directive

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,16 @@
 var triviaTimeApp = angular.module('triviaTimeApp', []);
+
+triviaTimeApp.controller('QuizController', function($scope) {
+	$scope.quizzes = [
+		{
+			name: 'Riddle Challenge',
+			rating: 3,
+			questions: []
+		},
+		{
+			name: 'AngularJS Concepts',
+			rating: 4,
+			questions: []
+		}
+	]
+});
