deb0175b67c4874e9089d0b3f528b6e2eb761f4a
Sarah Cnota <scnota@gmail.com>
2014-07-06 15:32:57 -0500

    update firebase url

--- app/js/services.js
+++ app/js/services.js
@@ -1,6 +1,6 @@
 var triviaServices = angular.module('triviaServices', []);
 
-triviaServices.constant('FIREBASE_URL','https://torid-fire-8241.firebaseio.com/questions');
+triviaServices.constant('FIREBASE_URL','https://wdiy-trivia.firebaseio.com/questions');
 
 triviaServices.factory('FirebaseService', function($firebase, FIREBASE_URL) {
 	return {
