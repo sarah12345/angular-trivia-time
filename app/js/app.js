88a26ee1956f9f33010c7a78d314fdb589fb5317
Sarah Cnota <scnota@gmail.com>
2014-07-06 12:55:58 -0500

    add functionality to add new questions

--- app/js/app.js
+++ app/js/app.js
@@ -6,6 +6,10 @@ triviaApp.config(['$routeProvider', function($routeProvider) {
 				templateUrl: 'partials/question-list.html',
 				controller: 'QuestionListCtrl'
 			}).
+			when('/questions/new', {
+				templateUrl: 'partials/question-new.html',
+				controller: 'QuestionNewCtrl'
+			}).
 			when('/questions/:questionId', {
 				templateUrl: 'partials/question-detail.html',
 				controller: 'QuestionDetailCtrl'
@@ -22,6 +26,18 @@ triviaApp.controller('QuestionListCtrl', ['$scope', '$firebase', function($scope
 	$scope.toggleAnswer = function(question) {
 		question.showAnswer = !question.showAnswer;
 	}
+}]);
+
+triviaApp.controller('QuestionNewCtrl', ['$scope', '$firebase', '$location', function($scope, $firebase, $location) {
+	$scope.question = {};
+
+	$scope.addQuestion = function(question) {
+		var firebaseUrl = "https://torid-fire-8241.firebaseio.com/questions";
+		$scope.questions = $firebase(new Firebase(firebaseUrl));
+		$scope.questions.$add(question).then(function(ref) {
+			$location.url('/questions');
+		});
+	};
 }]);
 
 triviaApp.controller('QuestionDetailCtrl', ['$scope', '$firebase', '$routeParams', function($scope, $firebase, $routeParams) {
