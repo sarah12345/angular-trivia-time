52d7e2e24c901d3e34abaecbc6b52cd1ba3ee17c
Sarah Cnota <scnota@gmail.com>
2014-07-19 20:11:19 -0500

    change firebase app

--- app/js/services.js
+++ app/js/services.js
@@ -1,6 +1,6 @@
 var triviaServices = angular.module('triviaServices', []);
 
-triviaServices.constant('FIREBASE_URL','https://wdiy-trivia2.firebaseio.com/questions');
+triviaServices.constant('FIREBASE_URL','https://wdiy-trivia-time.firebaseio.com/questions');
 
 triviaServices.factory('FirebaseService', function($firebase, FIREBASE_URL) {
 	return {
