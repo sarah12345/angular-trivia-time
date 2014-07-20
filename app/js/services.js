fc9488244438653d8159da212c94670df5b8752e
Sarah Cnota <scnota@gmail.com>
2014-07-19 23:18:06 -0500

    convert factory to service

--- app/js/services.js
+++ app/js/services.js
@@ -2,13 +2,11 @@ var triviaServices = angular.module('triviaServices', []);
 
 triviaServices.constant('FIREBASE_URL','https://wdiy-trivia-time.firebaseio.com/questions');
 
-triviaServices.factory('FirebaseService', function($firebase, FIREBASE_URL) {
-	return {
-		getQuestion: function(questionId) {
-			return $firebase(new Firebase(FIREBASE_URL + '/' + questionId));
-		},
-		getQuestions: function() {
-			return $firebase(new Firebase(FIREBASE_URL));
-		}
+triviaServices.service('FirebaseService', function($firebase, FIREBASE_URL) {
+	this.getQuestion = function(questionId) {
+		return $firebase(new Firebase(FIREBASE_URL + '/' + questionId));
+	};
+	this.getQuestions = function() {
+		return $firebase(new Firebase(FIREBASE_URL));
 	};
 });
